0958a5f069c5af851e67f16f4717b525ae52bcee
Omay-Edekar <omayedekar@gmail.com>
2018-12-14 13:21:42 +0000

    Changed grid.py to be 8x8 grid, not 8x16

--- grid.py
+++ grid.py
@@ -1,8 +1,8 @@
 for i in range(0, 100):
     print("\n")
-grid = [ ['__', ' '] * 8] * 8
+grid = [ ['__ '] * 8] * 8
 for i in range (0, 8):
-    for j in range(0, 16):
+    for j in range(0, 8):
         print(grid[i][j], end = ' ')
     print(i+1)
     print("\n")
