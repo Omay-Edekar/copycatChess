365c739482e54af1a8aa930b7fc50995da9e28d0
Omay-Edekar <omayedekar@gmail.com>
2018-12-14 13:09:27 +0000

    Added grid.py

---
+++ grid.py
@@ -0,0 +1,8 @@
+for i in range(0, 100):
+    print("\n")
+grid = [ ['__', ' '] * 8] * 8
+for i in range (0, 8):
+    for j in range(0, 16):
+        print(grid[i][j], end = ' ')
+    print(i+1)
+    print("\n")
